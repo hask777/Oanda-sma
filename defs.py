7ef10eaa94193c979b9e88d9eaacb149e66e5659
hask777 <haskellisp@gmail.com>
2023-03-15 13:48:34 +0300

    first commit

---
+++ defs.py
@@ -0,0 +1,8 @@
+API_KEY = "a3534840ee0ab2f77c0cafd7d386a93e-81013808fb855b500f2168f68b6b6de0"
+ACCOUNT_ID = "101-012-24390683-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
